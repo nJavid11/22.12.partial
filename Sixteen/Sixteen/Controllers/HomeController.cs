1c5fd3308edbcb0f286eca432d705dc68b97e008
nJavid11 <tu201803071@code.edu.az>
2021-12-22 20:39:14 +0400

    16 Clothing with Partial View

---
+++ Sixteen/Sixteen/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Sixteen.Data;
+using Sixteen.Models;
+using Sixteen.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sixteen.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly AppDBContext _context;
+
+        public HomeController(AppDBContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            VmHome model = new VmHome();
+            model.products = _context.Products.ToList();
+            model.sliders = _context.Sliders.ToList();
+
+            return View(model);
+        }
+    }
+}
---
+++ Sixteen/Sixteen/Controllers/ProductsController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Sixteen.Data;
+using Sixteen.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sixteen.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly AppDBContext _context;
+
+        public ProductsController(AppDBContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            VmProducts model = new VmProducts();
+            model.products = _context.Products.ToList();
+            model.head = _context.Heads.ToList().ElementAt(0);
+
+            return View(model);
+        }
+    }
+}
